76ab379529d77ca147c68c1f29555a8f5e354218
Sophie Gu <sophieqgu@gmail.com>
2018-08-03 20:36:52 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -186,7 +186,7 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(rows[1]).to eq("-----------")
       expect(rows[2]).to eq(" O | O | O ")
       expect(rows[3]).to eq("-----------")
-      expect(rows[4]).to eq(" O | O | X ")
+      expect(rows[4]).to eq(" O | O | O ")
       # Can you copy the syntax of the tests above to write a test for a board
       # entirely filled with Os?
 
