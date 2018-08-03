f686b446c07deeda2d3414e1c226998411054cb7
Sophie Gu <sophieqgu@gmail.com>
2018-08-03 20:36:47 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -176,6 +176,17 @@ describe "#display_board in 'lib/display_board.rb" do
     end
 
     it 'prints an entire board full of Os' do
+      
+      board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+      
+       output = capture_puts{ display_board(board) } if defined?(display_board)
+      rows = output.split("\n")
+      
+      expect(rows[0]).to eq(" O | O | O ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq(" O | O | O ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq(" O | O | X ")
       # Can you copy the syntax of the tests above to write a test for a board
       # entirely filled with Os?
 
